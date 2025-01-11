a11042aa0a155696fafc7e01d9cddde9119747c9
Anush Tenguriya <anushtenguriya93100@gmail.com>
2025-01-11 15:02:57 +0530

    Sam Marks

---
+++ Level1/SamMarks2.java
@@ -0,0 +1,22 @@
+public class SamMarks2{
+  public static void main(String[] args){
+     
+     // Sam's marks in physics
+     int physics=95;
+
+     // Sam's marks in chemistry
+     int chemistry=96;
+      
+     // Sam's marks in maths
+     int maths=94;
+     
+     // Sam's total marks in PCM
+     double total=physics + chemistry + maths;
+
+     //  Sam's average percentage
+     double percentage=total / 300 * 100;
+
+     // printing sam's percentage in PCM
+     System.out.println("percentage of Samin PCM is " + percentage);
+   }
+}
