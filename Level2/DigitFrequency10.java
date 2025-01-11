2a7889f82653717d3af8735e3b1b0edfcad140d3
Anush Tenguriya <anushtenguriya93100@gmail.com>
2025-01-11 16:39:32 +0530

    finding the frequency of digits

---
+++ Level2/DigitFrequency10.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class DigitFrequency10 {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        // Take input for a number
+        System.out.print("Enter a number: ");
+        long number = input.nextLong();
+
+        // Create an array to store the frequency of digits (0 to 9)
+        int[] frequency = new int[10];
+
+        // Find the digits in the number and update the frequency array
+        while (number != 0) {
+            int digit = (int) (number % 10);  
+            frequency[digit]++; 
+            number = number / 10;  
+        }
+
+        // Display the frequency of each digit
+        System.out.println("\nDigit Frequencies:");
+        for (int i = 0; i < 10; i++) {
+            if (frequency[i] > 0) {  // Only display digits that appear in the number
+                System.out.println("Digit " + i + ": " + frequency[i] + " times");
+            }
+        }
+
+        input.close();
+    }
+}
+
