ce496aaa3b23bdec4ad4ef3154e2fd0b27ff1396
Anush Tenguriya <anushtenguriya93100@gmail.com>
2025-01-11 16:06:35 +0530

    Question 2

---
+++ Level2/LeapYear2.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class LeapYear2 {
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        // taking the input from user
+        System.out.print("Enter a year: ");
+        int year = input.nextInt();
+
+        // Ensure the year is >= 1582 and check the leap year conditions in one if
+        // statement
+        if (year >= 1582 && ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0))) {
+            System.out.println(year + " is a Leap Year.");
+        } else {
+            System.out.println(year + " is not a Leap Year.");
+        }
+        input.close();
+    }
+}
