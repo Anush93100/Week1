48fbf0a6eccc08e2ecd0588a01ed25136e65d04b
Anush Tenguriya <anushtenguriya93100@gmail.com>
2025-01-11 15:05:03 +0530

    Converting kilo to miles

---
+++ Level1/KiloToMiles3.java
@@ -0,0 +1,13 @@
+public class KiloToMiles3{
+  public static void main(String[] args){
+    
+     //given Kilometers
+     double kilometer=10.8;
+
+     //calculating miles for given kilometers
+     double miles=kilometer / 1.6;
+
+     //printing distance in kilometer and miles
+     System.out.println("The distance " + kilometer + " km in miles is " + miles);
+  }
+}
