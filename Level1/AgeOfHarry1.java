d575a5dba56e65df77246e9be49e918ab6b8e7fd
Anush Tenguriya <anushtenguriya93100@gmail.com>
2025-01-11 14:47:53 +0530

    Finding afe of Harry

---
+++ Level1/AgeOfHarry1.java
@@ -0,0 +1,12 @@
+public class AgeOfHarry1{
+  public static void main(String[] args){
+
+    //age of Harry is 24
+    int age=24;
+
+    //printing Harry's current age
+    System.out.println("Age of Harry in 2024 is " + age);
+
+  
+  }
+}
