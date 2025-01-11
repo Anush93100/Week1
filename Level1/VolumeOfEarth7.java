e04ac9614aa489182db2a037ecba2d253b7ab992
Anush Tenguriya <anushtenguriya93100@gmail.com>
2025-01-11 15:12:11 +0530

    Question 7

---
+++ Level1/VolumeOfEarth7.java
@@ -0,0 +1,19 @@
+public class VolumeOfEarth7{
+	public static void main(String[] args){
+		 
+                 //given radius in kilometer
+		 double radiusInKm = 6378  ;
+                 
+                 //calculating radius in miles
+                 double radiusInMl = radiusInKm /1.6;
+		 
+                 //calculating volume in kilometer
+		 double volumeInKm =  3.14 * radiusInKm * radiusInKm * radiusInKm *4 /3;
+		 
+                 //calculating volume in miles
+		 double volumeInMl = 3.14 * radiusInMl * radiusInMl * radiusInMl *4 /3; ;
+		 
+                //displaying the output
+		 System.out.println("The volume of earth in cubic kilometers is " + volumeInKm + " and cubic miles is " + volumeInMl);
+	}
+}
